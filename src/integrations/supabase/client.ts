714d2d28f4950d330e17cbe3b4b45d3a20eeed89
Gabriel Odusanya <gabbytechsec@gmail.com>
2025-12-08 15:20:08 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = "https://jzsthgkivjfkdzcmxgeb.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_isYLPUMppzxWRzUQ3bl7xQ_Kd87j-iN";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
